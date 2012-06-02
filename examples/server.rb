35923ccde40b23d847c618af6519968c470ccbe8
Artem Yankov <artem@badgeville.com>
2012-06-02 12:23:38 -0700

    save to  mongo_mapper model

--- examples/server.rb
+++ examples/server.rb
@@ -1,6 +1,12 @@
 # vertx run examples/server.rb -cluster -cluster-port 25501
 
 require "../ruby/kover.rb"
+
+require 'mongo_mapper'
+
+MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+MongoMapper.database = "kover-test"
+
 require "examples/user.rb"
 require "examples/post.rb"
 
--- examples/user.rb
+++ examples/user.rb
@@ -1,15 +1,24 @@
 class User
+ include MongoMapper::Document
  include Kover::RPCable
 
- allow_rpc_for :create, :pop, :met1, :met2
+ allow_rpc_for :create, :find_by_name, :met1, :met2
+
+ key :first_name, String
+ key :last_name,  String
 
  class << self
    def create(user)
-    Redis.new.lpush('users', user.to_json)
+    u = self.new(user)
+    u.save!
+    u.as_json
    end
 
-   def pop
-    Redis.new.lpop('users')
+   def find_by_name(user)
+     p "!!!"
+     p user
+     u = User.where(:first_name => user['first_name']).first
+     u.try(:as_json)
    end
 
    def met1(a, b)
@@ -19,5 +28,13 @@ class User
     def met2(a,b)
       a * b
     end
+  end # class << self
+
+  def as_json
+    {
+      :first_name => self.first_name,
+      :last_name  => self.last_name
+    }
   end
-end
+
+end 
